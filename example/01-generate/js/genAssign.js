08dab6eaae772685509d8085e02294c31098b527
chullin <fg6ts15@gmail.com>
2018-03-09 09:11:53 +0800

    'finsifh'

--- example/01-generate/js/genAssign.js
+++ example/01-generate/js/genAssign.js
@@ -4,6 +4,7 @@ var next = U.print
 // === BNF Grammar =====
 // Assign = id '=' Exp
 // Exp = T ([+-] T)?     // 問號表示可以出現，或可以不出現(正規表達式) '*' 0次以上或無限 '+' 1次以上一定要出現
+                         // Exp T ;或是 Exp T +/- T
 // T = F ([*/] F)?       // F 乘除
 // F = [0-9] | (Exp)
 
@@ -21,16 +22,16 @@ function id () {
 }
 
 function Exp () {
-  T()
-  if (U.randInt(1, 10) <= 5) {
+  T()                                
+  if (U.randInt(1, 10) <= 5) {       //  T + or - T
     next(U.randChar('+-'))
     T()
   }
 }
 
 function T () {
-  F()
-  if (U.randInt(1, 10) <= 5) {
+  F()                                // T = F
+  if (U.randInt(1, 10) <= 5) {       // F * or /  F
     next(U.randChar('*/'))
     F()
   }
